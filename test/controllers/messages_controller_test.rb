5b5de2af4c287cd08683938b8d8167035a40c8e2
YurikOnRails <andrei.iurik@gmail.com>
2025-09-02 21:41:40 +0300

    fix: errors in tests

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,8 +1,15 @@
 require "test_helper"
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get messages_create_url
-    assert_response :success
+  test "paid user can create message" do
+    # user with standard plan from fixtures
+    sign_in users(:two)
+
+    assert_difference "Message.count", 2 do
+      # user message + AI response
+      post messages_path, params: { message: { content: "Hello from test" } }
+    end
+
+    assert_redirected_to root_path
   end
 end
