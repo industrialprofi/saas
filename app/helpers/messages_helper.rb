b6c5f6083bf394b765ecee6904656b0c72ce7a34
YurikOnRails <andrei.iurik@gmail.com>
2025-09-03 15:09:20 +0300

    lint: rubocop

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,6 +1,6 @@
 module MessagesHelper
   def quota_label(user)
-    return '' unless user
+    return "" unless user
 
     remaining = user.requests_remaining
     limit = user.daily_requests_limit
@@ -8,11 +8,11 @@ module MessagesHelper
   end
 
   def quota_text_classes(user)
-    base = 'mt-2 text-sm'
+    base = "mt-2 text-sm"
     return base unless user
 
     exhausted = user.quota_exhausted?
-    color = exhausted ? 'text-red-600' : 'text-gray-600 dark:text-gray-300'
+    color = exhausted ? "text-red-600" : "text-gray-600 dark:text-gray-300"
     "#{base} #{color}"
   end
 end
--- db/migrate/20250903144930_add_index_messages_on_user_id_created_at.rb
+++ db/migrate/20250903144930_add_index_messages_on_user_id_created_at.rb
@@ -1,5 +1,5 @@
 class AddIndexMessagesOnUserIdCreatedAt < ActiveRecord::Migration[7.1]
   def change
-    add_index :messages, [:user_id, :created_at]
+    add_index :messages, [ :user_id, :created_at ]
   end
 end
